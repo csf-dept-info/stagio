54815020c3163425a9d5d1687d253ba13881e736
chandonnet <franki_football@hotmail.com>
2015-02-05 09:24:03 -0500

    Création de l'entity Departmental Archives

---
+++ Stagio.Domain/Entities/DepartmentalArchives.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Stagio.Domain.Entities
+{
+    class DepartmentalArchives : Entity
+    {
+        public int StudentsCount { get; set; }
+
+        public int StudentsWithInternship { get; set; }
+
+        public int StudentsWithoutInternship { get; set; }
+
+        public int EmployeesCount { get; set; }
+
+        public int CompaniesCount { get; set; }
+
+        public int InternshipOffersCount { get; set; }
+
+        public DateTime PeriodStartDate { get; set; }
+
+        public DateTime PeriodEndDate { get; set; }
+
+        public int InterviewsCount { get; set; }
+
+        public int InternshipApplicationsCount { get; set; }
+
+        public int CompanyOffersCount { get; set; }
+    }
+}
