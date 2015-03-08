782858484ff35ba55e5648e25a5a48ad9e89f043
chandonnet <franki_football@hotmail.com>
2015-03-08 14:46:52 -0400

    Correction d'un WebMessage et suppression d'un dossier non utilisé

--- Stagio.Web/WebMessage/WebMessage.cs
+++ Stagio.Web/WebMessage/WebMessage.cs
@@ -75,7 +75,7 @@ namespace Stagio.Web
             public const string INTERNSHIP_OFFERS_COUNT = "Nombre total d'offres de stage offertes par les compagnies";
             public const string INTERVIEWS_COUNT = "Nombre total d'entrevues ayant eu lieu";
             public const string INTERNSHIP_APPLICATIONS_COUNT = "Nombre total d'applications sur les offres de stage par les étudiants";
-            public const string COMPANY_OFFERS_COUNT = "Nombre total de d'étudiants acceptés par les compagnies après une entrevue";
+            public const string COMPANY_OFFERS_COUNT = "Nombre total d'étudiants acceptés par les compagnies après une entrevue";
             public const string START_DATE = "Date de début de la période de stage";
             public const string END_DATE = "Date de fin de la période de stage";
         }
